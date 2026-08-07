2a153a286ddecf95cf67322cfccca262e4e0e794
fespina96 <fespina96@gmail.com>
2026-08-07 11:24:56 -0300

    Version Final Entrega

--- teapp-android/app/src/main/java/com/teapp/util/Constants.java
+++ teapp-android/app/src/main/java/com/teapp/util/Constants.java
@@ -2,7 +2,7 @@ package com.teapp.util;
 
 public final class Constants {
     // Cambiar a 10.0.2.2 para emulador, o a la IP local de la PC para dispositivo físico
-    public static final String BASE_URL = "http://192.168.1.34:8080/api/v1/";
+    public static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
 
     public static final String PREF_FILE = "teapp_prefs";
     public static final String PREF_TOKEN = "teapp_token";
